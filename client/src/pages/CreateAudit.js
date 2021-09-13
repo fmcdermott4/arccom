810d80b7be53a3d30ff8d32b67002a893f9e565d
Frank McDermott <fmcdermott4@gmail.com>
2021-09-13 14:27:06 -0400

    creat audit page dropdown working

--- client/src/pages/CreateAudit.js
+++ client/src/pages/CreateAudit.js
@@ -1,15 +1,56 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { useQuery } from '@apollo/client';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 import {READ_AUDIT_TYPES} from '../utils/queries'
-
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import Dropdown from 'react-bootstrap/Dropdown';
+import Form from 'react-bootstrap/Form'
 
 
 
 const CreateAudit = () => {
     const {loading, error, data} = useQuery(READ_AUDIT_TYPES);
+
+
+    const [auditType, setAuditType] = useState("");
+
+
+    const handleChange = (event) =>{
+        event.preventDefault();
+        console.log(event.target.value)
+    }
+    // Dropdown button for choosing audit type
+    const dropdownButton = (auditTypes) =>{
+        
+        
+        
+        const button = (auditTypes) =>{
+            return(auditTypes.map((audit)=>{
+                return(<option key={audit._id} value={audit._id}>{audit.name}</option>)
+            }))
+        }    
+            // const button = (auditTypes) =>{
+            //     return(
+            //         auditTypes.auditTypes.map((audit) => {
+            //             console.log(audit)
+            //             return(<option key={audit.auditType} value={audit.auditType}>{audit.auditType}</option>)
+            //         })
+            //     )
+            // }
+            
+        return(<Form><Form.Control as="select" defaultValue="OSHA" onChange={handleChange}>{button(auditTypes)}</Form.Control></Form>)
+        // <DropdownButton as={ButtonGroup} title="Select Audit Type" id="bg-nested-dropdown" onChange={handleChange}>
+        //     {auditTypes.map((auditType)=>{
+        //         return(<Dropdown.Item key={auditType._id} value={auditType._id}>{auditType.name}</Dropdown.Item>)
+        //     })}
+        //     <Dropdown.Item key="newAuditType" value="newAuditType">New Audit Type</Dropdown.Item>
+        // </DropdownButton>)
+    }
+
+
 
 
     if(error){
@@ -25,21 +66,19 @@ const CreateAudit = () => {
     return (
         <main>
             <Container>
-                <Row>
+                <Row className="justify-content-md-center" md="auto">
                     <h1>Create Audit Page</h1>
                 </Row>
                 <Row>
                     <Col>
-                        <div class="dropdown">
-                            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                Dropdown button
-                            </button>
-                            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                                <a class="dropdown-item" href="#">Action</a>
-                                <a class="dropdown-item" href="#">Another action</a>
-                                <a class="dropdown-item" href="#">Something else here</a>
-                            </div>
-                        </div>
+                        <Row>
+                            <Col md="auto">
+                                Choose Audit Type:
+                            </Col>
+                            <Col md="auto">                                
+                                {dropdownButton(data.auditTypes)}
+                            </Col>                                
+                        </Row>
                     </Col>                
                 </Row>
             </Container>
