6f4a4cb375ec5120df5c1a82ab4433d0a2b7bd09
Frank McDermott <fmcdermott4@gmail.com>
2021-09-15 15:17:14 -0400

    select audit pushing to conduct audit page

--- client/src/pages/SelectAuditToConduct.js
+++ client/src/pages/SelectAuditToConduct.js
@@ -1,14 +1,14 @@
-import React, {useState} from 'react';
+import React, {useState } from 'react';
 import {useQuery} from '@apollo/client';
 import {READ_AUDIT_TYPES, READ_AUDITS_TO_CONDUCT_BY_AUDIT_TYPE} from '../utils/queries';
 import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
+
 
 const SelectAuditToConduct = () => {
     
-    const [auditType, setAuditType] = useState("");
+    const [auditType, setAuditType] = useState(null);
 
     const {loading, data} = useQuery(READ_AUDIT_TYPES);
 
@@ -26,16 +26,15 @@ const SelectAuditToConduct = () => {
         return(
             <Form>
                 <Form.Group as={Row}>
-                    <Form.Control as="select" onChange={handleSelect}>
-                        
-                            {
-                                auditType.map((audit)=>{
-                                    return(
-                                        <option name={audit._id} key={audit.name} value={audit._id}>{audit.name}</option>
-                                    )
-                                }) 
-                            }
-                    
+                    <Form.Control as="select" onChange={handleSelect} defaultValue="">
+                    <option disabled key="" value="">Select One</option>
+                        {
+                            auditType.map((audit)=>{
+                                return(
+                                    <option name={audit._id} key={audit.name} value={audit._id}>{audit.name}</option>
+                                )
+                            }) 
+                        }                    
                     </Form.Control>
                 </Form.Group>
             </Form>
@@ -48,42 +47,56 @@ const SelectAuditToConduct = () => {
     return(
         <div>
             Select audit type {auditTypeButton(data.auditTypes)}
-             <SelectAuditFromTypes data={auditType} />
+            {(auditType)?<SelectAuditFromTypes data={auditType} /> : <div />}
         </div>
     )
 }
 
 const SelectAuditFromTypes = (auditType) =>{
-    console.log(auditType.data.auditType);
     const auditTypeId = auditType.data.auditType;
-    // console.log(auditType.data.auditType);
-    const { loading, data } = useQuery(READ_AUDITS_TO_CONDUCT_BY_AUDIT_TYPE, 
+    const { loading, data, error } = useQuery(READ_AUDITS_TO_CONDUCT_BY_AUDIT_TYPE, 
         {variables: 
             {"auditType": auditTypeId},
         }
     );
 
-        
+    const [auditToConduct, setAuditToConduct] = useState("");
     
+    const handleSelect = async (event) => {
+        event.preventDefault();
+        const {value} = event.target;
+        await setAuditToConduct({
+            auditToConduct: value
+        });
+    }
+
     if(loading){
         return<div>Loading...</div>
     }
+    if(error || data.auditsToConductByAuditType.length === 0){
+        return<div></div>
+    }
     return(
         <Form>
-            {console.log(data.auditsToConductByAuditType)}
+            {console.log(auditToConduct)}
             <Form.Label>Select Audit</Form.Label>
-                <Form.Group as={Row}>
-                    <Form.Control as="select" >                        
-                        {
-                            data.auditsToConductByAuditType.map((audit)=>{
-                                return(
-                                    <option name={audit._id} key={audit.name} value={audit._id}>{audit.name}</option>
-                                )
-                            }) 
-                        }                    
-                    </Form.Control>
-                </Form.Group>
-            </Form>
+            <Form.Group as={Row}>
+                <Form.Control as="select" onChange={handleSelect} defaultValue="">
+                    <option disabled key="" value="">Select One</option>                        
+                    {
+                        data.auditsToConductByAuditType.map((audit)=>{
+                            return(
+                                <option name={audit._id} key={audit.name} value={audit._id}>{audit.name}</option>
+                            )
+                        }) 
+                    }                    
+                </Form.Control>
+            </Form.Group>
+            <br />
+            <Row>                
+                {(auditToConduct !== "")?<Button href={'/audits/conductaudit/' + auditToConduct.auditToConduct}>Conduct Audit</Button>:<div />}
+            </Row>
+        </Form>
     )
     
 
